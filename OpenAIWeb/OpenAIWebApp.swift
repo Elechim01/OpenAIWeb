7629185f26ea9122bd779df9a0c4189409ac5a81
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-12-31 22:54:05 +0100

    Initial Commit

---
+++ OpenAIWeb/OpenAIWebApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAIWebApp.swift
+//  OpenAIWeb
+//
+//  Created by Michele Manniello on 31/12/22.
+//
+
+import SwiftUI
+
+@main
+struct OpenAIWebApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
